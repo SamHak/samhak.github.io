f87054f483bb83d6f94765f56f333a9a67cb4d58
Sam Hakobyan <Sam.hak1995@gmail.com>
2017-09-19 21:11:10 +0400

    add

--- js/headerButton.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-    var buttonHeader = document.querySelector('.header__menu');
-    var dropdownHeader = document.querySelector('.header__menu_list');
-
-    buttonHeader.onclick = function(){
-        dropdownHeader.classList.toggle("header__menu_list-active");
-    };
-};
