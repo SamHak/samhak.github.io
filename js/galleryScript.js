f4f09c88171cb137f4d7dbccfefc819a52d0cb33
Sam Hakobyan <Sam.hak1995@gmail.com>
2017-09-22 23:52:15 +0400

    add new site contact + fix map

---
+++ js/galleryScript.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    var buttonHeader = document.querySelector('.header__menu');
+    var dropdownHeader = document.querySelector('.header__menu_list');
+
+    buttonHeader.onclick = function(){
+        dropdownHeader.classList.toggle("header__menu_list-active");
+    };
+
+};
